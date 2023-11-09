9c5d55400e9d074ba354e2991e4f03b40a75539c
Eoghan Hynes <A00107408@student.ait.ie>
2023-11-09 12:45:45 +0000

    Update Colors.cs

--- Assets/KociembaTwoPhase/Colors.cs
+++ Assets/KociembaTwoPhase/Colors.cs
@@ -1,1 +1,26 @@
+/* <summary
+ * 
+ * Augmented Reality Rubik Cube Application
+ * A.I.T 2018
+ * A00107408
+ * Masters by Research
+ * 
+ * File Description:
+ * Enumeration for facelet colors in terms
+ * of face direction.
+ * U = Green Face
+ * R = REd Face
+ * F = Yellow Face etc 
+ * 
+ * Acknowledgments:
+ * Original @author: Elias Frantar.
+ * Translated Kociemba algorithm to java.
+ *   
+ * <summary> */
+
+//++++++++++++++++++++++++++++++ Names the colors of the cube facelets ++++++++++++++++++++++++++++++++++++++++++++++++
+public enum Colors
+{
+    U, R, F, D, L, B
+}
 
